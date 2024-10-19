441a30345a43bd55a7f4e0f9503ca22d71c079fe
Ilonica <95592035+Ilonica@users.noreply.github.com>
2024-10-19 23:02:49 +0300

    Задание 1.1 - создан класс Student, прописаны конструктор, геттер и сеттер для каждого поля

---
+++ Lab1/Student.kt
@@ -0,0 +1,45 @@
+class Student(var lastName: String, var firstName: String, var middleName: String) {
+    private var _id: Int? = null
+    var id: Int?
+        set(value) {
+            _id = value
+        }
+        get() = _id
+
+    private var _phone: String? = null
+    var phone: String?
+        set(value) {
+            _phone = value
+        }
+        get() = _phone
+
+    private var _telegram: String? = null
+    var telegram: String?
+        set(value) {
+            _telegram = value
+        }
+        get() = _telegram
+
+    private var _email: String? = null
+    var email: String?
+        set(value) {
+            _email = value
+        }
+        get() = _email
+
+    private var _git: String? = null
+    var git: String?
+        set(value) {
+            _git = value
+        }
+        get() = _git
+
+    constructor(lastName: String, firstName: String, middleName: String, _id: Int? = null, _phone: String? = null, _telegram: String? = null, _email: String? = null, _git: String? = null)
+            : this(lastName, firstName, middleName) {
+        id = _id
+        phone = _phone
+        telegram = _telegram
+        email = _email
+        git = _git
+    }
+}
