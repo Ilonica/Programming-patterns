dcba1e5ae9dc8daee9bdab8238b37591ed530b33
Ilonica <95592035+Ilonica@users.noreply.github.com>
2024-12-03 17:25:53 +0300

    Задание 2.2.6 - реализован паттерн Шаблон для класса DataList

--- Lab2/DataList.kt
+++ Lab2/DataList.kt
@@ -11,7 +11,18 @@ abstract class DataList<T>(val elements: Array<T>) {
     // Метод для получения массива ID выделенных элементов
     fun getSelectedIds() = selectedIndices.toIntArray()
     // Метод для получения массива наименований атрибутов, кроме ID, не работает и будет реализован в наследниках
-    abstract fun getNames(): Array<String>
+    open fun getNames(): Array<String>{
+        throw UnsupportedOperationException("Метод должен быть реализован во вложенном классе")
+    }
     // Метод для получения объекта DataTable, не работает и будет реализован в наследниках
-    abstract fun getData(): DataTable<T>
+    open fun getData(): DataTable<String>{
+        val data = fetchData()
+        val newData = formatData(data)
+        return DataTable(newData)
+    }
+
+    // Метод для извлечение данных
+    abstract fun fetchData() : Array<Array<String?>>
+    // Метод для форматирования данных
+    abstract fun formatData(data:Array<Array<String?>>): Array<Array<String>>
 }
